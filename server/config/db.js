c4e62b72e02fb1a7689f8d4cef3abe63c2fc6b34
Tanmay <tanmaysharma763@gmail.com>
2025-01-08 15:18:12 +0530

    dwada

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,9 @@ const { query } = require("express");
 const connectDB = async () => {
     // strict query
   mongoose.set("strictQuery", true);
-  
+  app.get("/",(req,res)=>{
+    res.send("hello world");
+  })
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(
