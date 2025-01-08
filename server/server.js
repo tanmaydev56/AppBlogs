ac12309599bfb24381bfd07a3766aea954a3221c
Tanmay <tanmaysharma763@gmail.com>
2025-01-08 15:38:14 +0530

    wdad

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db");
 
 //env config
@@ -15,10 +16,10 @@ const blogRoutes = require("./routes/blogRoutes");
 //mongodb connection
 connectDB();
 
-//rest objecct
+//rest object
 const app = express();
 
-//middelwares
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
@@ -27,12 +28,22 @@ app.use(morgan("dev"));
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/blog", blogRoutes);
 
+// Serve static files from the React client
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Port
 const PORT = process.env.PORT || 8080;
+
 //listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
+    `Server Running on ${process.env.NODE_ENV} mode port no ${PORT}`.bgCyan
       .white
   );
 });
